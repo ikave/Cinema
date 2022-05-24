987295d12803fca4582f1a3b3ea7584a1d366e5d
ikave <shiftgear@mail.ru>
2022-05-24 11:47:21 +0300

    Добавляет моковые данные на главную страницу

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -4,6 +4,6 @@ import App from './components/app/app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title='The Grand Budapest Hotel' genre='Drama' release='2014' image='img/bg-the-grand-budapest-hotel.jpg' poster='img/the-grand-budapest-hotel-poster.jpg' />
   </React.StrictMode>,
   document.getElementById('root'));
