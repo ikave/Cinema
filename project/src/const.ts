5d42d80a0aa19b215f6fd20f78dc2554743688e3
ikave <shiftgear@mail.ru>
2022-05-30 15:37:46 +0300

    Изменяет статусы авторизации

--- project/src/const.ts
+++ project/src/const.ts
@@ -8,10 +8,11 @@ export const AppRoute = {
   NOT_FOUND: '/404',
 };
 
-export const AuthStatus = {
-  AUTH: 'AUTH',
-  NO_AUTH: 'NO_AUTH',
-};
+export enum AuthStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
 
 export const reviewStars = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
