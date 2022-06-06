4d5ef660e37e1234ec6f198ea005a42b44f0354d
ikave <shiftgear@mail.ru>
2022-06-06 11:21:22 +0300

    Переходит на r-r-d v6

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,7 @@
-import {Route, Switch} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import { Route, Routes } from 'react-router-dom';
+import browserHistory from '../../browser-history';
+import { AppRoute } from '../../const';
+import { useAppSelector } from '../../hooks';
 import AddReview from '../../pages/add-review/add-review';
 import Film from '../../pages/film/film';
 import MainPage from '../../pages/main-page/main-page';
@@ -7,8 +9,9 @@ import MyList from '../../pages/my-list/my-list';
 import NotFound from '../../pages/not-found/not-found';
 import Player from '../../pages/player/player';
 import Signin from '../../pages/signin/signin';
-import {FilmType} from '../../types/film';
-import {ReviewsType} from '../../types/reviews';
+import { FilmType } from '../../types/film';
+import { ReviewsType } from '../../types/reviews';
+import HistoryRouter from '../history-route/history-route';
 import ProtectedRoute from '../protected-route/protected-route';
 
 interface AppProps {
@@ -17,33 +20,30 @@ interface AppProps {
   movie: FilmType;
 }
 
-function App({favoritesFilms, movie, reviews}: AppProps): JSX.Element {
+function App({ favoritesFilms, movie, reviews }: AppProps): JSX.Element {
+  const auth = useAppSelector((state) => state.USER.authorizationStatus);
   return (
-    <Switch>
-      <Route path={AppRoute.MAIN} exact>
-        <MainPage />
-      </Route>
-      <Route path={AppRoute.SIGNIN} exact>
-        <Signin />
-      </Route>
-      <Route path={AppRoute.FILM} exact>
-        <Film movie={movie} />
-      </Route>
-      <Route path={AppRoute.PLAYER} exact>
-        <Player movie={movie} />
-      </Route>
-      <Route path={AppRoute.ADD_REVIEW} exact>
-        <AddReview movie={movie} />
-      </Route>
-      <ProtectedRoute
-        exact
-        path={AppRoute.MY_LIST}
-        render={() => <MyList films={favoritesFilms} />}
-      />
-      <Route path="*">
-        <NotFound />
-      </Route>
-    </Switch>
+    <HistoryRouter history={browserHistory}>
+      <Routes>
+        <Route path={AppRoute.MAIN} element={<MainPage />} />
+        <Route path={AppRoute.SIGNIN} element={<Signin />} />
+        <Route path={AppRoute.FILM} element={<Film movie={movie} />} />
+        <Route path={AppRoute.PLAYER} element={<Player movie={movie} />} />
+        <Route
+          path={AppRoute.ADD_REVIEW}
+          element={<AddReview movie={movie} />}
+        />
+        <Route
+          path={AppRoute.MY_LIST}
+          element={
+            <ProtectedRoute authorizationStatus={auth}>
+              <MyList films={favoritesFilms} />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HistoryRouter>
   );
 }
 
